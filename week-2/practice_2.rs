ec698b12e7790f33fa921e532219f0cc50749f1b
kurama859 <kelechukwu.urama@pau.edu.ng>
2026-09-02 23:04:12 +0100

    (Project 1,2 & 3)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,8 @@
+fn main() {
+	println!(); // prints just a
+	println!("hello");//prints
+	println!("format {} arguments", "some"); //prints format some arguments
+	println!("My name is {}. i am a SEN student", "Urama Kelechukwu-Jumbo")
+
+
+}
---
+++ week-2/practice_3.rs
@@ -0,0 +1,6 @@
+fn main() {
+	let fees =25_000;
+	let salary:f64 = 35_000.00;
+	let wages = 15_000;
+	println!("fees is {}, salary is {} and wages is {} ",fees,salary,wages);
+}
